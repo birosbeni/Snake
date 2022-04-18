35aa52f3e7420328d875e1faeb70115fe381087c
birosbeni <Beni>
2022-04-18 19:40:58 +0200

    making food with random effect

---
+++ Snake/SupriseFood.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    internal class SupriseFood : PictureBox
+    {
+        public static int appleSize = SnakePiece.snakeSize;
+        public SupriseFood()
+        {
+            this.Width = appleSize;
+            this.Height = appleSize;
+
+            this.BackColor = Color.BlueViolet;
+        }
+    }
+}
